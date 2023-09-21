f4ee0917f03e118c1c56843913e8e93ce94d7955
Sathvuka Adulapuram <sathvukaadulapuram@Sathvukas-MacBook-Pro.local>
2023-09-21 15:59:04 +0530

    Maven with POM

---
+++ Youtube_POM/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $mavenpom.testng;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
